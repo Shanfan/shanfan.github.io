9acea70b46045c8f6000cd348ed7aaedfbdb243a
Shanfan Huang <Shanfan@users.noreply.github.com>
2024-02-15 14:06:46 -0500

    Add a tag cloud

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 import projectData from "./projectMeta.json" assert {type: 'json'}
 
 const wrapper = document.querySelector(".projects-wrapper")
+const tagCounts = {}
 
 projectData.forEach(d => {
     const card = document.createElement('a')
@@ -24,8 +25,30 @@ projectData.forEach(d => {
         const span = document.createElement('span')
         span.innerHTML = tag
         tags.appendChild(span)
+
+        tagCounts[tag] = (tagCounts[tag] || 0) + 1
     })
 
     card.appendChild(tags)
     wrapper.appendChild(card)
 })
+
+const tagsAll = Object.keys(tagCounts).map(tag => {
+    return { tag: tag, count: tagCounts[tag] }
+})
+
+const tagCloud = document.querySelector(".tag-cloud")
+
+tagsAll.forEach(t => {
+    const tagBtn = document.createElement("button")
+    tagBtn.innerHTML = t.tag + "(" + t.count + ")"
+    tagBtn.style.fontSize = 1 + Math.log10(t.count) + "em"
+    tagBtn.onclick = () => filterProject(t.tag)
+
+    tagCloud.appendChild(tagBtn)
+})
+
+const filterProject = (tag) => {
+    console.log(tag + " is clicked!")
+}
+
